519f27ed38df1508c5b9dd6a97a2e547aa0b046a
erblinaberisha <75163819+erblinaberisha@users.noreply.github.com>
2021-05-19 19:58:00 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,105 +1,51 @@
 <!DOCTYPE html>
 <html lang="en"><!-- Basic -->
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-	  
-    <!-- Mobile Metas -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-     <!-- Site Metas -->
-    <title>Food House Restaurant| A cozy, nostalgic Kosovo café and wine bar in Kosovo, KS </title>  
-    <meta name="keywords" content="Wine Bar, West Village brunch, West Village lunch, Vegetables, Cocktails, West Village">
-    <meta name="description" content="Nostalgic Kosovo café and wine bar in the Balkan with seasonal European-American cuisine, classic cocktails, cozy setting, open every day.">
-    <meta name="author" content="">
-
-	<!-- Site Icons -->
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">    
-	<!-- Site CSS -->
-	<link rel="stylesheet" href="css/style.css"> 
-	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<link href="css/baguetteBox.min.css" rel="stylesheet">
-	<link href="css/superslides.css" rel="stylesheet">
-    <!-- Responsive CSS -->
-	<link rel="stylesheet" href="css/responsive.css">
-</head>
-
-<body>
-
+	<link rel="stylesheet" href="bootstrap.min.css">
 	<!-- Start header -->
-	<header class="top-navbar">  
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container">
-				<a class="navbar-brand" href="index.html">
-					<img src="images/logo.png" alt="">
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-				  <span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbars-rs-food">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Menu</a>
-							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="food.html">Food</a>
-								<a class="dropdown-item" href="drinks.html">Drinks</a>
-								<a class="dropdown-item" href="desserts.html">Desserts</a>
-							</div>
-						</li>
-						<li class="nav-item"><a class="nav-link" href="online_order.html">Online Order</a></li>
-						<li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-						<li class="nav-item"><a class="nav-link" href="reservation.html">Reservation</a></li>
-						<li class="nav-item"><a class="nav-link" href="staff.html">Staff</a></li>
-						<li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
-						<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</header>
+	<?php
+	if(!file_exists('assets/headernav.php') || !file_exists('assets/footer.php')){
+		exit('The file was not included correctly in this website. Maybe that file does not exist.');
+	}
+	include('assets/headernav.php');
+	?>
 	<!-- End header -->
 	
 	<!-- Start slides -->
 	<div id="slides" class="cover-slides">
 		<ul class="slides-container">
 			<li class="text-left">
-				<img src="images/slider-01.jpg" alt="">
+				<img src="slider-01.jpg" alt="">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Food House Restaurant</strong></h1>
 							<p class="m-b-40">A cozy, nostalgic restaurant and wine bar in the heart of the Kosovo</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.html">Reservation</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.php">Reservation</a></p>
 						</div>
 					</div>
 				</div>
 			</li>
 			<li class="text-left">
-				<img src="images/slider-02.jpg" alt="">
+				<img src="slider-02.jpg" alt="">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Food House Restaurant</strong></h1>
 							<p class="m-b-40">"Nothing brings people together like good food" <br>"Laughter is brightest where food is best"</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.html">Reservation</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.php">Reservation</a></p>
 						</div>
 					</div>
 				</div>
 			</li>
 			<li class="text-left">
-				<img src="images/slider-03.jpg" alt="">
+				<img src="slider-03.jpg" alt="">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Food House Restaurant</strong></h1>
 							<p class="m-b-40">GOOD FOOD <br> IS <br> GOOD MOOD</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.html">Reservation</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.php">Reservation</a></p>
 						</div>
 					</div>
 				</div>
@@ -141,11 +87,11 @@
 						food and drink. We know that by ensuring we create and offer the best quality products, we improve the lives of our family and yours. We've enjoyed over our years of business in Prishtina, We are taking the traditions and recipes
 						from our home and bringing the "family Food style House" to our restaurant. We know that Food tastes better when you eat with your family, and we consider all of our staff and customers famigilia."Feel like home" </p>
 						<p class=" gradient"><strong>We hope to welcome you soon.</strong></p>
-						<a class=" btn btn-lg btn-circle btn-outline-new-white" href="about.html">Our Story</a>
+						<a class=" btn btn-lg btn-circle btn-outline-new-white" href="about.php">Our Story</a>
 					</div>
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12">
-				    <img src="images/about-img.jpg" alt="" class="img-fluid imgfluid">
+				    <img src="about-img.jpg" alt="" class="img-fluid imgfluid">
 		        </div>
 			</div>
 		</div>
@@ -182,11 +128,11 @@
 							<h1> Special Menu</h1>
 							<p> Our kitchen is rooted in an appreciation for exceptional homegrown ingredients, thoughtful-yet-simple preparations, and a passion for breathing new life into Old World recipes. Desserts borrow from treasured family recipes extending
 								back four generations. In addition to the menus listed here, we offer a selection of daily specials.</p>
-							<a class="btn btn-lg btn-circle btn-outline-new-white" href="food.html">Menu</a>
+							<a class="btn btn-lg btn-circle btn-outline-new-white" href="food.php">Menu</a>
 						</div>
 					</div>
 					<div class="col-lg-6 col-md-6 col-sm-12">
-						<img src="images/menu.jpg" alt="" class="img-fluid imgfluid">
+						<img src="menu.jpg" alt="" class="img-fluid imgfluid">
 					</div>
 				</div>
 			</div>
@@ -200,7 +146,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-6 col-md-6 col-sm-12">
-						<img src="images/gallery-img-02.jpg" alt="" class="img-fluid imgfluid">
+						<img src="gallery-img-02.jpg" alt="" class="img-fluid imgfluid">
 					</div>
 					<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 						<div class="inner-column1">
@@ -208,7 +154,7 @@
 							<br>
 							<h1>Gallery</h1>
 							<p> Food House Restaurant is founded in a passion for exceptional food, drink, service, and hospitality with heart. </p>
-							<a class="btn btn-lg btn-circle btn-outline-new-white" href="gallery.html">Gallery</a>
+							<a class="btn btn-lg btn-circle btn-outline-new-white" href="gallery.php">Gallery</a>
 						</div>
 					</div>
 				</div>
@@ -233,7 +179,7 @@
 						<div class="carousel-inner mt-4">
 							<div class="carousel-item text-center active">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/quotations-button.png" alt="">
+									<img class="d-block w-100 rounded-circle" src="quotations-button.png" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Blerina Luma</strong></h5>
 								<h6 class="text-dark m-0">Happy Customer</h6>
@@ -242,7 +188,7 @@
 							</div>
 							<div class="carousel-item text-center">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/quotations-button.png" alt="">
+									<img class="d-block w-100 rounded-circle" src="quotations-button.png" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Tringe Dema</strong></h5>
 								<h6 class="text-dark m-0">Happy Customer</h6>
@@ -250,7 +196,7 @@
 							</div>
 							<div class="carousel-item text-center">
 								<div class="img-box p-1 border rounded-circle m-auto">
-									<img class="d-block w-100 rounded-circle" src="images/quotations-button.png" alt="">
+									<img class="d-block w-100 rounded-circle" src="quotations-button.png" alt="">
 								</div>
 								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Ard Nika</strong></h5>
 								<h6 class="text-dark m-0">Happy Customer</h6>
@@ -277,95 +223,16 @@
 		longitude:<span id="longitude"></span>&deg;
 	</p>-->
 	<!-- Start Contact info -->
-	<div class="contact-imfo-box" >
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 arrow-right">
-					<i class="fa fa-volume-control-phone"></i>
-					<div class="overflow-hidden">
-						<h4>Phone</h4>
-						<address class="lead">
-							+383-49-001-001
-						</address>
-					</div>
-				</div>
-				<div class="col-md-4 arrow-right">
-					<i class="fa fa-envelope"></i>
-					<div class="overflow-hidden">
-						<h4>Email</h4>
-						<address class="lead">
-							Foodhouse113@gmail.com
-						</address>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<i class="fa fa-map-marker"></i>
-					<div class="overflow-hidden">
-						<h4>Location</h4>
-						<address class="lead">
-							 Prishtina KS
-						</address>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	
 	<!-- End Contact info -->
 	
 	<!-- Start Footer -->
-	<footer class="footer-area bg-f" >
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3 col-md-6" style="margin-right: 100px;">
-					<h3>About Us</h3>
-					<p>Since our opening, we have become masters of our craft. We deliver good quality, food with great service and incomparable customer care keep our community coming back again and again.</p>
-
-				</div>
-			
-				<div class="col-lg-3 col-md-6" style="margin-right: 100px;">
-					<h3>Contact information</h3>
-					<address class="lead">Prishtina , 10000</address>
-					<address class="lead">+383-49-001-001</address>
-					<address><a href="mailto:Foodhouse113@gmail.com">Foodhouse113@gmail.com </a></address>
-					<ul class="list-inline f-social">
-						<li class="list-inline-item"><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Ffood.house2021%2F%3Ffbclid%3DIwAR0wkBllp5izksEj4fPQYR-xV0qo8wrGH3ENQ2FiW6tAGugikeXNBio7IYo&h=AT3APLLmE7NeOzpKFRYyPHxnkGCxeJ63YQu144Mpcfc3srPpmnF3iKg43a0VqtqX7j6HtG5OULuSZcRWyYXLFs6DkiFesE-qQkfv7_HjxkmPT_TTT-0hEUalSR8GPW7nXZn-5LJwVGg" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-							</ul>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<h3>Opening hours</h3>
-					<p><span class="text-color">Mon-Tue :</span> 9AM-11PM</p>
-					<p><span class="text-color">Wed-Thu :</span> 9:AM-11PM</p>
-                    <p><span class="text-color">Fri-Sat :</span> 9:AM-11PM</p>
-                    <p><span class="text-color">Sunday :</span> 5PM-12PM </p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="copyright">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<p class="company-name">All Rights Reserved. &copy;2021 <a href="index.html">Food House Restaurant</a></p>
-					</div>
-				</div>
-            </div>
-		</div>
-		
-	</footer>
+	<?php
+	include('assets/footer.php');
+	?>
 	<!-- End Footer -->
 	
-	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
-
-	<!-- ALL JS FILES -->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-    <!-- ALL PLUGINS -->
-	<script src="js/jquery.superslides.min.js"></script>
-	<script src="js/custom.js"></script>
-	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-	<script>
-		AOS.init();
-	  </script>
+	
     <script>
 		if('geolocation'in navigator){
 		   console.log('geolocation available');
