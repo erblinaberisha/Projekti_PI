bbac05f122e9f60da3f0541f418b7bf159805168
erblinaberisha <75163819+erblinaberisha@users.noreply.github.com>
2021-05-19 20:09:34 +0200

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,93 @@
+    <div class="contact-imfo-box" >
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<address class="lead">
+							+383-49-001-001
+						</address>
+					</div>
+				</div>
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<address class="lead">
+						foodhouserestaurant2021
+						</address>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-map-marker"></i>
+					<div class="overflow-hidden">
+						<h4>Location</h4>
+						<address class="lead">
+							 Prishtina KS
+						</address>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<footer class="footer-area bg-f" >
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6" style="margin-right: 100px;">
+                <h3>About Us</h3>
+                <p>Since our opening, we have become masters of our craft. We deliver good quality, food with great service and incomparable customer care keep our community coming back again and again.</p>
+
+            </div>
+        
+            <div class="col-lg-3 col-md-6" style="margin-right: 100px;">
+                <h3>Contact information</h3>
+                <address class="lead">Prishtina , 10000</address>
+                <address class="lead">+383-49-001-001</address>
+                <address><a href="mailto:foodhouserestaurant2021@gmail.com">foodhouserestaurant2021@gmail.com</a></address>
+                <ul class="list-inline f-social">
+                    <li class="list-inline-item"><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Ffood.house2021%2F%3Ffbclid%3DIwAR0wkBllp5izksEj4fPQYR-xV0qo8wrGH3ENQ2FiW6tAGugikeXNBio7IYo&h=AT3APLLmE7NeOzpKFRYyPHxnkGCxeJ63YQu144Mpcfc3srPpmnF3iKg43a0VqtqX7j6HtG5OULuSZcRWyYXLFs6DkiFesE-qQkfv7_HjxkmPT_TTT-0hEUalSR8GPW7nXZn-5LJwVGg" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h3>Opening hours</h3>
+                <p><span class="text-color">Mon-Tue :</span> 9AM-11PM</p>
+                <p><span class="text-color">Wed-Thu :</span> 9:AM-11PM</p>
+                <p><span class="text-color">Fri-Sat :</span> 9:AM-11PM</p>
+                <p><span class="text-color">Sunday :</span> 5PM-12PM </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="company-name">All Rights Reserved. &copy;2021 <a href="index.php">Food House Restaurant</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</footer>
+<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="jquery-3.2.1.min.js"></script>
+	<script src="bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="jquery.superslides.min.js"></script>
+	<script src="custom.js"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="contact-form-script.js"></script>
+    <script src="form-validator.min.js"></script>
+	<script src="name.form.js"></script>
+	<script src="images-loded.min.js"></script>
+	<script src="myform_phone.js"></script>
+	<script src="form.address.js"></script>
+	<script src="lastname.form.js"></script>
+	<script src="date.form.js"></script>
+	<script src="time.form.js"></script>
+	<script>
+		AOS.init();
+	</script>
